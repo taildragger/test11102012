4d726578bc22efab495e7d4df515e8727602fd38
Taildragger <taildragger@nope.com>
2012-11-10 23:06:36 -0800

    nitial commit

---
+++ app/models/note.rb
@@ -0,0 +1,4 @@
+class Note < ActiveRecord::Base
+   attr_accessible :name, :text, :link
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = '3e43f27a4a538a6a5a68df2dba6555f1e294829dabc933d7e4ffb994ae22deb3ed10738319b7d3a654f58560006c807c11f5f72badfeee89abb0a47312f36b8e'
